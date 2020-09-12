400ccdc45b428163e4a8f0a2d1f678dc73cee266
JimSunJing <jimsun6428@gmail.com>
2020-09-12 14:50:39 +0800

    exercise 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Detail from './Detail'
 
 const Country = ({countries, searchKey}) => {
+    const [btnShow, setBtnShow] = useState({})
+
     const res = countries.filter(country => 
         country.name.toLowerCase().indexOf(searchKey) !== -1)
 
@@ -10,8 +12,19 @@ const Country = ({countries, searchKey}) => {
             {res.length > 10 
                 ? <div>Too many matches, specify another filter</div>
                 : res.length === 1 
-                    ? <Detail country={res[0]} />
-                    : res.map(c => <div key={c.name}>{c.name}</div>) }
+                    ?  <Detail country={res[0]} /> 
+                    : res.map(c => 
+                        <div key={c.name}>
+                            {c.name}
+                            <button onClick={() => setBtnShow(c)}>
+                                show
+                            </button>
+                        </div>
+                    ) 
+            }
+            {btnShow.name !== undefined 
+                ? <Detail country={btnShow} />
+                : <div></div> }
         </div>
     );
 };
