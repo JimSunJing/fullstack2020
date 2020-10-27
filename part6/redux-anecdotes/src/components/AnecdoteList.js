cbb0c5d454b7ec343eec1feae4c0da80bfe47519
JimSunJing <jimsun6428@gmail.com>
2020-10-28 00:18:17 +0800

    exercise 6.12

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,8 +4,16 @@ import { genVote } from '../reducers/anecdoteReducer';
 import { notificationChange } from '../reducers/notificationReducer';
 
 const AnecdoteList = () => {
-  
-  const anecdotes = useSelector(state => state.anecdotes)
+
+  const anecdotes = useSelector(state => {
+    if (!state.filter || state.filter === '') {
+      return state.anecdotes
+    }else {
+      return state.anecdotes.filter(a => {
+        return a.content.indexOf(state.filter) !== -1
+      })
+    }
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -14,23 +22,23 @@ const AnecdoteList = () => {
     dispatch(notificationChange(`you voted "${anecdotes.find(a => a.id === id).content}"`))
     setTimeout(() => {
       dispatch(notificationChange(''))
-    },5000)
+    }, 5000)
   }
   return (
     <div>
       {anecdotes
-        .sort((a,b) => b.votes - a.votes)
+        .sort((a, b) => b.votes - a.votes)
         .map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -7,12 +7,14 @@ import App from './App'
 
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
 
 // import { notificationChange } from './reducers/notificationReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
-  notification: notificationReducer
+  notification: notificationReducer,
+  filter: filterReducer
 })
 
 const store = createStore(
