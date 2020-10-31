ee0c048e7100078f0d1683b18bd303ff2ea5469c
JimSunJing <jimsun6428@gmail.com>
2020-10-31 21:07:43 +0800

    exercise 6.19

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
 import { genVote } from '../reducers/anecdoteReducer'
 // import { notificationChange } from '../reducers/notificationReducer'
 import { pushNotification } from '../reducers/notificationReducer'
+import { connect } from 'react-redux'
 
-const AnecdoteList = () => {
+const AnecdoteList = (props) => {
 
-  const anecdotes = useSelector(state => {
-    if (!state.filter || state.filter === '') {
-      return state.anecdotes
-    }else {
-      return state.anecdotes.filter(a => {
-        return a.content.indexOf(state.filter) !== -1
-      })
-    }
-  })
-  const dispatch = useDispatch()
+  // const anecdotes = useSelector(state => {
+  //   if (!state.filter || state.filter === '') {
+  //     return state.anecdotes
+  //   }else {
+  //     return state.anecdotes.filter(a => {
+  //       return a.content.indexOf(state.filter) !== -1
+  //     })
+  //   }
+  // })
+  // const dispatch = useDispatch()
 
   const vote = (id) => {
     console.log('vote', id)
-    dispatch(genVote(id))
-    dispatch(pushNotification(`you voted "${anecdotes.find(a => a.id === id).content}"`, 10))
+    props.genVote(id)
+    props.pushNotification(`you voted "${props.anecdotes.find(a => a.id === id).content}"`, 10)
   }
   return (
     <div>
-      {anecdotes
+      {props.anecdotes
         .sort((a, b) => b.votes - a.votes)
         .map(anecdote =>
           <div key={anecdote.id}>
@@ -41,4 +42,30 @@ const AnecdoteList = () => {
   );
 };
 
-export default AnecdoteList;
+// export default AnecdoteList;
+
+const mapStateToProps = state => {
+  if (!state.filter || state.filter === '') {
+    return {
+      anecdotes: state.anecdotes
+    }
+  } else {
+    return {
+      anecdotes: state.anecdotes.filter(a => {
+        return a.content.indexOf(state.filter) !== -1
+      })
+    }
+  }
+}
+
+const mapDispatchToProps = {
+  genVote,
+  pushNotification
+}
+
+const ConnectedAnecdoteList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AnecdoteList)
+
+export default ConnectedAnecdoteList
